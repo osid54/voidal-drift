8c1b1b1d7902954f94ea4af0c75b74b42f004309
osid54 <osid.potato@gmail.com>
2024-05-15 21:36:52 -0400

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+@export var speed := 300.0
+@export var accel := 50
+@export var friction := 20
+
+func _physics_process(_delta):
+	var direction = Input.get_vector("left", "right","up","down")
+	
+	if direction.y > 0:
+		$AnimatedSprite2D.play("front")
+	elif direction.y < 0:
+		$AnimatedSprite2D.play("back")
+	if direction.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif direction.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	
+	if direction:
+		velocity = velocity.move_toward(Vector2(speed,speed)*direction, accel)
+	else:
+		velocity = velocity.move_toward(Vector2(), friction)
+
+	move_and_slide()
